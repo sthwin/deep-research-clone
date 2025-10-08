f7002a572088505aefd64632fc837c59a6b29ffa
“sthwin” <sthwin@naver.com>
2025-10-09 00:19:25 +0900

    Initial commit: Deep research clone project setup

---
+++ tools.py
@@ -0,0 +1,44 @@
+import os
+import re
+
+from firecrawl import FirecrawlApp, ScrapeOptions
+
+
+def web_search_tool(query: str):
+    """
+    Web Search Tool
+    Args:
+        query: str
+            The query to search the web for
+    Returns:
+        A list of search results with the website content in markdown format
+    """
+    app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+
+    response = app.search(
+        query=query,
+        limit=1,
+        scrape_options=ScrapeOptions(
+            formats=["markdown"],
+        ),
+    )
+
+    if not response.success:
+        return "Error using tool: " + response.error
+
+    cleaned_chunks = []
+
+    for result in response.data:
+
+        title = result["title"]
+        url = result["url"]
+        markdown = result["markdown"]
+
+        cleaned = re.sub(r"\\+|\n+", "", markdown).strip()
+        cleaned = cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
+
+        cleaned_chunk = {"title": title, "url": url, "markdown": cleaned}
+
+        cleaned_chunks.append(cleaned_chunk)
+
+    return cleaned_chunks
